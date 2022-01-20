1c8c76664671be0439948117b3e722036cb649fd
kongpop1235 <81024350+kongpop1235@users.noreply.github.com>
2022-01-20 21:30:13 +0700

    Enter

--- src/components/core/Detail.vue
+++ src/components/core/Detail.vue
@@ -242,71 +242,90 @@
     </v-card-text>
     <v-card-text>
       <v-row id="row_main" v-for="(item, index) in main" :key="index">
-        <v-col
-          cols="12"
-          class="grey--text text--lighten-1 d-flex bg_card rounded-lg position-relative mb-6"
-        >
-          <v-card-text class="d-flex align-center my-0 py-0 pl-2">
-            <v-img
-              max-height="90"
-              max-width="90"
-              :src="item.api_img"
-              class="mr-4"
-            ></v-img>
-            <div class="text_list">
-              <p class="primary--text mb-1">
-                <span class="text-h6">{{ item.item }}</span>
-              </p>
-              <p class="mb-1">
-                BUY : <span :style="item.city_color_buy">{{ item.city_buy_price }}</span>
-              </p>
-              <p class="mb-1">
-                SELL :
-                <span :style="item.city_color_sell">{{ item.city_sell_price }}</span>
-              </p>
-            </div>
-            <v-spacer></v-spacer>
-            <div class="green--text mb-2 mr-2 text-right">
-              <p class="mb-2">+ {{ item.percent }}%</p>
-              <p class="mr-3 text-h4 d-inline">+{{ item.profit }}</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                width="16"
-                height="16"
-                viewBox="0 0 172 172"
-                style="fill: #000000"
-                class="d-inline"
-              >
-                <g
-                  fill="none"
-                  fill-rule="nonzero"
-                  stroke="none"
-                  stroke-width="1"
-                  stroke-linecap="butt"
-                  stroke-linejoin="miter"
-                  stroke-miterlimit="10"
-                  stroke-dasharray=""
-                  stroke-dashoffset="0"
-                  font-family="none"
-                  font-weight="none"
-                  font-size="none"
-                  text-anchor="none"
-                  style="mix-blend-mode: normal"
+        <v-hover v-slot="{ hover }">
+          <v-col
+            cols="12"
+            class="grey--text text--lighten-1 d-flex bg_card rounded-lg position-relative mb-6 pl-0"
+          >
+            <v-card-text class="d-flex align-center my-0 py-0 pl-5">
+              <v-img
+                max-height="90"
+                max-width="90"
+                :src="item.api_img"
+                class="mr-4"
+              ></v-img>
+              <div class="text_list">
+                <p class="primary--text mb-1">
+                  <span class="text-h6">{{ item.item }}</span>
+                </p>
+                <p class="mb-1">
+                  BUY :
+                  <span :style="item.city_color_buy">{{ item.city_buy_price }}</span>
+                </p>
+                <p class="mb-1">
+                  SELL :
+                  <span :style="item.city_color_sell">{{ item.city_sell_price }}</span>
+                </p>
+              </div>
+              <v-spacer></v-spacer>
+              <div class="green--text mb-2 mr-2 text-right">
+                <p class="mb-2">+ {{ item.percent }}%</p>
+                <p class="mr-3 text-h4 d-inline">+{{ item.profit }}</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 172 172"
+                  style="fill: #000000"
+                  class="d-inline"
                 >
-                  <path d="M0,172v-172h172v172z" fill="none"></path>
-                  <g fill="#4caf50">
-                    <path d="M5.8738,152.65l80.1262,-138.7825l80.1262,138.7825z"></path>
-                    <path
-                      d="M86,18.1675l76.4024,132.3325h-152.8048l76.4024,-132.3325M86,9.5675l-83.85,145.2325h167.7l-83.85,-145.2325z"
-                    ></path>
+                  <g
+                    fill="none"
+                    fill-rule="nonzero"
+                    stroke="none"
+                    stroke-width="1"
+                    stroke-linecap="butt"
+                    stroke-linejoin="miter"
+                    stroke-miterlimit="10"
+                    stroke-dasharray=""
+                    stroke-dashoffset="0"
+                    font-family="none"
+                    font-weight="none"
+                    font-size="none"
+                    text-anchor="none"
+                    style="mix-blend-mode: normal"
+                  >
+                    <path d="M0,172v-172h172v172z" fill="none"></path>
+                    <g fill="#4caf50">
+                      <path d="M5.8738,152.65l80.1262,-138.7825l80.1262,138.7825z"></path>
+                      <path
+                        d="M86,18.1675l76.4024,132.3325h-152.8048l76.4024,-132.3325M86,9.5675l-83.85,145.2325h167.7l-83.85,-145.2325z"
+                      ></path>
+                    </g>
                   </g>
-                </g>
-              </svg>
-            </div>
-          </v-card-text>
-        </v-col>
+                </svg>
+              </div>
+            </v-card-text>
+            <v-expand-transition>
+              <router-link
+                to="/Detail"
+                v-if="hover"
+                class="d-flex transition-fast-in-fast-out pa pl-0 darken-2 v-card--reveal white--text text-h text-uppercase font-weight-bold rounded-lg text-decoration-none"
+                style="height: 100%"
+              >
+                <div
+                  
+                  class="d-flex justify-center align-center"
+                  style="height: 100%; width: 100%"
+                >
+                  <p>Details</p>
+                </div>
+              </router-link>
+            </v-expand-transition>
+          </v-col>
+        </v-hover>
       </v-row>
     </v-card-text>
   </v-card>
@@ -373,7 +392,9 @@ export default {
         });
       } else {
         this.main.sort(function (cc, dd) {
-          return parseFloat(dd.city_buy_price_check) - parseFloat(cc.city_buy_price_check);
+          return (
+            parseFloat(dd.city_buy_price_check) - parseFloat(cc.city_buy_price_check)
+          );
         });
       }
       this.low = false;
@@ -449,7 +470,9 @@ export default {
             alert("max || min || low");
             const lowmm = this.$store.getters[this.hds];
             lowmm.sort(function (e, f) {
-              return parseFloat(e.city_buy_price_check) - parseFloat(f.city_buy_price_check);
+              return (
+                parseFloat(e.city_buy_price_check) - parseFloat(f.city_buy_price_check)
+              );
             });
             this.main = [];
             for (let sls = 0; sls < lowmm.length; sls++) {
@@ -466,7 +489,9 @@ export default {
             alert("max || min || high");
             const highmm = this.$store.getters[this.hds];
             highmm.sort(function (j, k) {
-              return parseFloat(k.city_buy_price_check) - parseFloat(j.city_buy_price_check);
+              return (
+                parseFloat(k.city_buy_price_check) - parseFloat(j.city_buy_price_check)
+              );
             });
             this.main = [];
             for (let shs = 0; shs < highmm.length; shs++) {
